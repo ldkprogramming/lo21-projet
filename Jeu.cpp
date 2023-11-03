d25abb5e48f2c8226b441c74a9de91b8f933d609
Leonardo <ldkcompsci@gmail.com>
2023-11-03 20:47:14 +0100

    Ebauche de la classe Jeu

--- Jeu.cpp
+++ Jeu.cpp
@@ -3,3 +3,19 @@
 //
 
 #include "Jeu.h"
+Jeu::Handler Jeu::handler = Handler();
+
+
+/* a definir plus en detail
+Jeu& Jeu::getInstance() {
+    if (handler.instance == nullptr) {
+        handler.instance = new Jeu;
+    }
+    return *handler.instance;
+}
+
+void Jeu::libererInstance() {
+    delete handler.instance;
+    handler.instance = nullptr;
+}
+*/
--- Jeu.h
+++ Jeu.h
@@ -5,8 +5,44 @@
 #ifndef LO21_PROJET_JEU_H
 #define LO21_PROJET_JEU_H
 
+#include "Sac.h"
+#include "Pioche.h"
+
 
 class Jeu {
+private:
+    /*
+     * On utilise un pattern Singleton, implemente avec un struct internet Handler
+     *
+     *
+     * */
+    Jeu();
+    ~Jeu();
+
+    struct Handler {
+        Jeu* instance;
+        Handler() : instance(nullptr){};
+        ~Handler() {
+            delete instance;
+        }
+    };
+    static Handler handler;
+
+    Sac sac = Sac();
+    // PlateauJetons ...
+    // PlateauCartes
+    Pioche pioche = Pioche();
+
+
+public:
+
+
+    static Jeu& getInstance();
+    static void libererInstance();
+    Jeu(const Jeu& j) = delete;
+    Jeu& operator=(const Jeu& j) = delete;
+
+
 
 };
 
