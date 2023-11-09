b536354ab749e5462711e615c91b93d9acdf5315
Tareq <149778732+TareqD5@users.noreply.github.com>
2023-11-09 15:01:42 +0100

    Create splendor.h

---
+++ splendor.h
@@ -0,0 +1,43 @@
+#ifndef SPLENDORDUEL_SPLENDOR_H
+#define SPLENDORDUEL_SPLENDOR_H
+#pragma once
+
+#include <iostream>
+#include <string>
+
+// Pour l'aléatoire
+#include <stdlib.h>
+#include <time.h>
+
+// Pour récuperer les cartes depuis la Base de données
+#include "sqlite3.h"
+
+// Pour que les différentes classes puissent communiquer entre elles 
+#include "carte.h"
+#include "controlleur.h"
+#include "IA.h"
+#include "Jeton.h"
+#include "Jeu.h"
+#include "Joueur.h"
+#include "Pioche.h"
+#include "PlateauJetons.h"
+#include "Privilege.h"
+#include "sac.h"
+
+using namespace std;
+
+
+
+namespace Splendor {
+
+    // Classe pour gérer les exceptions
+    class SetException {
+    public:
+        SetException(const string& i) :info(i) {}
+        string getInfo() const { return info; }
+    private:
+        string info;
+    };
+}
+
+#endif //SPLENDORDUEL_SPLENDOR_H
